db28ffdfdf5353ff8fe69da88afd61ca2f4a43dd
Stefano Borzi <stefano.borzi@fedex.com>
2021-12-19 00:06:11 +0100

    feat: add cache for github API

--- src/app/services/catalogue/catalogue.service.ts
+++ src/app/services/catalogue/catalogue.service.ts
@@ -1,31 +1,57 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Config } from './catalogue.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Config } from "./catalogue.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CatalogueService {
-
   CONF: Config;
   items$: { [key: string]: Observable<any> } = {};
 
-  constructor(
-    private http: HttpClient,
-  ) {
+  constructor(private http: HttpClient) {
     try {
-      this.CONF = require('src/assets/config.json');
+      this.CONF = require("src/assets/config.json");
     } catch {
-      this.CONF = require('src/assets/default.json');
+      this.CONF = require("src/assets/default.json");
     }
 
     for (const k of Object.keys(this.CONF.tabs)) {
-      this.items$[k] = this.getItems(this.CONF.tabs[k]);
-    };
+      this.items$[k] = this.getLocalItems(this.CONF.tabs[k]);
+    }
   }
 
-  getItems(githubTopic: string): Observable<any> {
-    return this.http.get(`https://api.github.com/search/repositories?page=${this.CONF.page}&per_page=${this.CONF.perPage}&q=${this.CONF.ORGANIZATION}fork:true+topic:${githubTopic}+sort:stars`);
+  private getLocalItems(githubTopic: string): Observable<any> {
+    const key = `${this.CONF.page}-${this.CONF.perPage}-${this.CONF.ORGANIZATION}-${githubTopic}`;
+    const item = localStorage.getItem(key);
+
+    if (item && !this.expireMinutes(30, JSON.parse(item)["timeDate"])) {
+      return of(JSON.parse(item)["value"]);
+    }
+
+    return this.getItems(githubTopic, key);
+  }
+
+  getItems(githubTopic: string, key: string): Observable<any> {
+    return this.http
+      .get(
+        `https://api.github.com/search/repositories?page=${this.CONF.page}&per_page=${this.CONF.perPage}&q=${this.CONF.ORGANIZATION}fork:true+topic:${githubTopic}+sort:stars`
+      )
+      .pipe(
+        tap((data) => {
+          localStorage.setItem(
+            key,
+            JSON.stringify({ timeDate: new Date().getTime(), value: data })
+          );
+        })
+      );
+  }
+
+  private expireMinutes(minutes: number, timeDate: number): boolean {
+    const diff = Math.abs(new Date().getTime() - timeDate);
+    const diffMinutes = Math.floor(diff / 1000) / 60;
+    return minutes < diffMinutes;
   }
 }
