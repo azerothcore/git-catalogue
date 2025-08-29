2adc04571dbd9f1f6d29ebe87f079b967a7dfe6f
Yehonal <yehonal.azeroth@gmail.com>
2025-08-29 18:09:39 +0200

    Remove usePreGeneratedFile from environment configurations

--- src/environments/environment.e2e.ts
+++ src/environments/environment.e2e.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  production: false,
-  // Force use of pre-generated local file during E2E to avoid external APIs
-  usePreGeneratedFile: true
+  production: false
 };
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  usePreGeneratedFile: true  // Use pre-generated file in production to reduce API calls
+  production: true
 };
